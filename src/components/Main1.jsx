43d51966918aa039b1dd0736a46d21567f73e1e8
yashesh-04 <yasheshshah04@gmail.com>
2024-10-16 18:32:09 +0530

    Fix image import path

--- src/components/Main1.jsx
+++ src/components/Main1.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import '/src/assets/profile.jpg'
+import React from 'react';
+import profileImage from '../assets/profile.jpg'; // Correct the relative path based on your structure
 
-export default function Main1(){
-    return(
+
+export default function Main1() {
+    return (
         <div className='main'>
-            <img className="profile" src="src/assets/profile.jpg" alt="" />
+            <img className="profile" src={profileImage} alt="Profile" /> {/* Use the imported image */}
             <h2 className='intro'>Hi! I'm Yashesh Shah 👋</h2>
             <h1 className='designation'>frontend web developer<br/>based in India.</h1>
             <p className='description'>I am a frontend developer from Karnataka, India with multiple <br/>years of experience.</p>
             <button className='contact-me' onClick={() => window.location.href = 'mailto:yasheshshah04@gmail.com'}>Contact Me →</button>
-
             <button className='my-resume' onClick={() => window.open("https://drive.google.com/file/d/1QNd4VJBujS3ti7FlfsI8_UPpMfWTBg77/view?usp=sharing", "_blank")}>My Resume 📥</button>
-
         </div>      
-    )
+    );
 }
