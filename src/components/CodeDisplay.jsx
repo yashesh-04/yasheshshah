5df8e5b77cfc1d3f7cc3b390f44ef395991ae1cb
yashesh-04 <111443277+yashesh-04@users.noreply.github.com>
2025-03-02 17:06:31 +0530

    Update CodeDisplay.jsx

--- src/components/CodeDisplay.jsx
+++ src/components/CodeDisplay.jsx
@@ -2,48 +2,13 @@ import React from "react";
 
 const CodeDisplay = () => {
   const codeSnippet = `
-  import { Link, useNavigate, useLocation } from "react-router-dom";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+  import { Navigate } from "react-router-dom";
 
-const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
-  const navigate = useNavigate();
-  const location = useLocation(); // Get the current URL path
-
-  const handleLogout = () => {
-    toast.error("Logged out!!", { position: "top-right", autoClose: 2000 });
-
-    setTimeout(() => {
-      setIsLoggedIn(false);
-      navigate("/");
-    }, 2000); // Wait for toast to disappear before redirecting
-  };
-
-  return (
-    <nav className="navbar">
-      <h1 className="logo">📚 Online Bookstore</h1>
-      <div className="nav-links">
-        {isLoggedIn ? (
-          <>
-            <Link to="/home">Home</Link>
-            <Link to="/book-of-the-day">Book of the Day</Link>
-            <Link to="/cart">Cart</Link>
-            {/* Hide Logout button on the Login page */}
-            {location.pathname !== "/login" && (
-              <button className="logout-btn" onClick={handleLogout}>
-                Logout
-              </button>
-            )}
-          </>
-        ) : (
-          location.pathname !== "/login" && <Link to="/login">Login</Link>
-        )}
-      </div>
-    </nav>
-  );
+const PrivateRoute = ({ isLoggedIn, children }) => {
+  return isLoggedIn ? children : <Navigate to="/login" />;
 };
 
-export default Navbar;
+export default PrivateRoute;
 
   `;
 
